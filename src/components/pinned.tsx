d3de102913ecbb553255a5972f375ca4b15c8d44
PavlovIvan1 <pavlov-2011pavlov@yandex.ru>
2025-05-02 15:41:07 +0300

    fix: telegram init

--- src/components/pinned.tsx
+++ src/components/pinned.tsx
@@ -1,10 +1,17 @@
 import { Project } from './project'
 
 export function Pinned() {
-	return (
-		<>
-		  <h2 style={{color: '#1E1E1E', fontWeight: '500',  margin: '0', marginTop: '20px'}}>Pinned project</h2>
-			<Project />
-		</>
-	)
+  return (
+    <>
+      <h2 style={{color: '#1E1E1E', fontWeight: '500', margin: '0', marginTop: '20px'}}>
+        Pinned project
+      </h2>
+      <Project 
+        name="Project Name" 
+        description="Project description" 
+        budget={1000000} 
+        status="in_progress" 
+      />
+    </>
+  )
 }
